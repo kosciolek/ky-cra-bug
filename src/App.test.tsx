b8e45c14c6c51ab1e0db06da0cf331f039fb2714
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-16 15:36:19 +0200

    .

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,21 +6,27 @@ import App from "./App";
 test("renders learn react link", async () => {
   const Comp = () => {
     useEffect(() => {
-      ky
-        .get("https://api.exchangerate.host/latest")
-        .then((r) => {
-          console.log('no shortcut status', r.status);
-          return r.json();
-        })
-        .then((data) => console.log("no shortcut", data));
 
-      (async () => {
+      function runLongForm() {
+        ky
+          .get("https://api.exchangerate.host/latest")
+          .then((r) => {
+            console.log('no shortcut status', r.status);
+            return r.json();
+          })
+          .then((data) => console.log("no shortcut", data));
+      }
 
+      async function runShortForm() {
         console.log(
           "json shortcut",
           await ky.get("https://api.exchangerate.host/latest").json()
         );
-      })();
+      }
+
+      runLongForm();
+      runShortForm();
+
     }, []);
 
     return null;
