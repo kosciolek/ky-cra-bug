76e8cb1f43eb9b8386ee3713246428afb3c9016f
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-16 15:35:19 +0200

    .

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import ky from "ky";
+import React, { useEffect } from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders learn react link", async () => {
+  const Comp = () => {
+    useEffect(() => {
+      ky
+        .get("https://api.exchangerate.host/latest")
+        .then((r) => {
+          console.log('no shortcut status', r.status);
+          return r.json();
+        })
+        .then((data) => console.log("no shortcut", data));
+
+      (async () => {
+
+        console.log(
+          "json shortcut",
+          await ky.get("https://api.exchangerate.host/latest").json()
+        );
+      })();
+    }, []);
+
+    return null;
+  };
+
+  render(<Comp />);
+
+  await new Promise((res) => setTimeout(res, 500));
+}, 20000);
